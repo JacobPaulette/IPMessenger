b624250927ff0c76c9cd4e06b95185baf6145305
Jacob Paulette <jacobpaulette@gmail.com>
2017-05-29 12:53:53 -0400

    gui tests

--- gui.cpp
+++ gui.cpp
@@ -13,13 +13,35 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+GUI::GUI(std::queue<Ticket>& to_gui, std::queue<Ticket>& to_system) {
+    this->to_gui = to_gui;
+    this->to_system = to_system;
+
+    initscr();
+
+    int y, x;
+    getmaxyx(stdscr, y, x);
+    cbreak();
+    noecho();
+    input_win = newwin(1, x,y-1,0);
+    output_win = newwin(y-1,x,0,0);
+    to_system.push(Ticket(Ticket_Type::quit));
+    return;
+    while (running) {
+        handle_ticket();
+        recv_char_input();
+        refresh();
+    }
+    endwin();
+}
+
 void GUI::handle_ticket() {
-    if (to_gui->empty())
+    if (to_gui.empty())
         return;
     else {
-        Ticket current_ticket = to_gui->front();
+        Ticket current_ticket = to_gui.front();
         Ticket_Type type = current_ticket.get_type();
-        to_gui->pop();
+        to_gui.pop();
         if (type == Ticket_Type::recv_message) 
             print_message(current_ticket);
     }
@@ -37,7 +59,33 @@ void GUI::print_to_window(WINDOW * win, string text) {
         wechochar(win, i);
 }
 
-void GUI::enter_handle() {}
+void GUI::cat_output(string message) {
+    if (message == "quit") {
+        to_system.push(Ticket(Ticket_Type::quit));
+        running = false;
+    }
+    vector<IP4> delta;
+    string delims = " \"";
+    vector<string> message_list = split(message, delims);
+    for (int i=0; i<message_list.size(); i++) {
+        if (message_list[i] == "-ip") {
+            delta.push_back(IP4(message_list[i+1]));
+            to_system.push(Ticket(delta));
+            return;
+        }
+    }
+    to_system.push(Ticket(message));
+}
+
+
+void GUI::enter_handle() {
+    wmove(input_win,0,0);
+    for (int i=0; i<out_message.size(); i++)
+        wechochar(input_win, ' ');
+    wmove(input_win,0,0);
+    cat_output(out_message);
+    out_message = "";
+}
 
 void GUI::backspace_handle() {}
 
@@ -58,7 +106,7 @@ void GUI::recv_char_input() {
 
 void gui(queue<Ticket>* to_gui, queue<Ticket>* to_system) {
    GUI(*to_gui, *to_system);
-   string msg;
+   /*string msg;
    IP4 me("127.0.0.1");
    vector<IP4> l = {me};
    to_system->push(Ticket(l));
@@ -73,5 +121,29 @@ void gui(queue<Ticket>* to_gui, queue<Ticket>* to_system) {
        while (to_gui->empty()) {};
        cout << "out : " << (to_gui->front()).get_message() << endl;
        to_gui->pop();
-   }
+   }*/
 }
+
+
+bool isMember(char elem, string & delims) {
+    for (char i : delims) {
+        if (elem == i)
+            return true;
+    }
+    return false;
+}
+
+vector<string> split(string & str, string & delims) {
+    vector<string> out;
+    string segment = "";
+    for (char i : str) {
+         if (!isMember(i, delims));
+        else {
+            out.push_back(segment);
+            segment = "";
+        }
+    }
+    return out;
+}
+            
+            
--- gui.h
+++ gui.h
@@ -10,7 +10,7 @@
 class GUI {
     public:
         GUI();
-        GUI(std::queue<Ticket>& to_gui, std::queue<Ticket>& to_system) {};
+        GUI(std::queue<Ticket>& to_gui, std::queue<Ticket>& to_system);
         void set_recv_queue(std::queue<Ticket>* tqueue);
         void set_send_queue(std::queue<Ticket>* tqueue);
         void stop();
@@ -18,11 +18,12 @@ class GUI {
 
     private:
         std::string out_message;
-        std::queue<Ticket> * to_gui;
-        std::queue<Ticket> * to_system;
+        std::queue<Ticket> to_gui;
+        std::queue<Ticket> to_system;
         WINDOW * output_win;
         WINDOW * user_handle_win;
         WINDOW * input_win;
+        bool running;
 
         void print_message(Ticket & current_ticket);
         void print_to_window(WINDOW * win, std::string text);
@@ -30,9 +31,13 @@ class GUI {
         void recv_char_input();
         void backspace_handle();
         void enter_handle();
-        void cat_input(std::string message);
+        void cat_output(std::string message);
 };
 
 void gui(std::queue<Ticket>* to_gui, std::queue<Ticket>* to_system);
+
+bool isMember(char elem, std::string & delims);
+
+std::vector<std::string> split(std::string & str, std::string  & delims);
 
 #endif
